d68caa91d033ba28721a1f492f10fd2d6450a8dc
isaach1 <isaachudis1@gmail.com>
2020-06-18 14:57:27 +0100

    Adding new pages (Isaac)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/isaac', to: 'pages#isaac'
+  get '/new_page', to: 'pages#new_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
