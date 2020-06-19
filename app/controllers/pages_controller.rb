ece9b376671a3bd674744e2b2317cb3b7fe40ab1
Peter Gramaglia <peter.gramaglia@gmail.com>
2020-06-18 22:23:38 -0400

    Added Peter's page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+  def peter
+    
+  end
+  def gramaglia_peter
+    render "peter"
+    puts "=" * 50
+    puts "hi mom"
+    puts "=" * 50
+  end
 end
