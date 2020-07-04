4847c17df5a09a7cb583d32a36475033c90d45d5
isaach1 <isaachudis1@gmail.com>
2020-07-04 14:04:04 +0100

    form data working

--- app/controllers/pdf_processor_controller.rb
+++ app/controllers/pdf_processor_controller.rb
@@ -17,8 +17,19 @@ class PdfProcessorController < ApplicationController
 
   def upload_pdf 
     pdf_processor = params[:file]
+    title = params[:title]
+    author = params[:author]
+    subject = params[:subject]
+    date = params[:date]
+    keywords = params[:keywords]
     # byebug
+    puts "uploading pdf"
     puts pdf_processor.original_filename
+    puts title
+    puts author
+    puts subject
+    puts date
+    puts keywords
   end
 
 end
